24682a5dd791938e14a2adae6c575a37d003c758
Gunshoot <Grrisha2593@gmail.com>
2020-08-15 11:55:34 +0300

    task1

--- task1/SRC/itoBase.cpp
+++ task1/SRC/itoBase.cpp
@@ -25,23 +25,25 @@ int checkBase(std::string str)
 std::string number_case(int num) {
 	switch (num) 
 	{
-		case 0: return "0";
-		case 1: return "1";
-		case 2: return "2";
-		case 3: return "3";
-		case 4: return "4";
-		case 5: return "5";
-		case 6: return "6";
-		case 7: return "7";
-		case 8: return "8";
-		case 9: return "9";
-		case 10: return "a";
-		case 11: return "b";
-		case 12: return "c";
-		case 13: return "d";
-		case 14: return "e";
-		case 15: return "f";
+		case 0: return ("0");
+		case 1: return ("1");
+		case 2: return ("2");
+		case 3: return ("3");
+		case 4: return ("4");
+		case 5: return ("5");
+		case 6: return ("6");
+		case 7: return ("7");
+		case 8: return ("8");
+		case 9: return ("9");
+		case 10: return ("a");
+		case 11: return ("b");
+		case 12: return ("c");
+		case 13: return ("d");
+		case 14: return ("e");
+		case 15: return ("f");
 	}
+
+	return ("Not valid symbol !");
 }
 
 std::string itoBase(unsigned int nb, std::string base)
